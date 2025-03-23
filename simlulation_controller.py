d1b957d9185955fa16bf39a900dccd621647d2a0
runzexiao <runzexiao@gmail.com>
2025-03-23 16:57:22 +0900

    All filename-related bugs have been fixed, a README.md has been added, and simulation_controller.py has been modified to standardize the benchmarking workflow.

--- simlulation_controller.py
+++ simlulation_controller.py
@@ -2,32 +2,36 @@ from coppeliasim_zmqremoteapi_client import RemoteAPIClient
 import time
 import os
 
+
+# environment_type can be 'lessthan30' or 'morethan30'
+environment_type = "lessthan30"
+#environment_num can be in range from 1 to 50 
+environment_num = 1
+#method can be choosed from ['my_method','my_method_pathplan','constantly_soft','constantly_soft_pathplan','distribute','distribute_pathplan','motorized_joint','motorized_joint_pathplan'], # Here my_method means MRS-CWC with TDT
+method = 'constantly_soft_pathplan'
+
 client = RemoteAPIClient()
 sim = client.require('sim')
 start = 0
 sim.setStepping(True)
-# environment = ['lessthan30','morethan30']
-environment = ["morethan30"]
-#method = ['my_method','my_method_pathplan','constantly_soft','constantly_soft_pathplan','distribute','distribute_pathplan','motorized_joint','motorized_joint_pathplan']
-method = ['my_method']
-for meth in method:
-    script_dir = os.path.dirname(os.path.abspath(__file__)) 
-    scenePath = os.path.join(script_dir, "coppeliasim_model", f"{meth}.ttt")
-    sim.setStringSignal("method", meth)
-    for env in environment:
-        sim.setStringSignal("environment", env)
-        for i in range(0,50):
-            j = i + 1
-            sim.setStringSignal("terrain_number", j)
-            sim.loadScene(scenePath)
-            time.sleep(1.5) 
-            sim.startSimulation()
-            while True:
-                sim.step()
-                simState = sim.getSimulationState()
-                if simState == sim.simulation_paused:
-                    break
-                #time.sleep(0.1)  
-            print(f"Simulation about {env},{meth},terrain number{j} is done.")    
-            sim.stopSimulation()
-       
+meth = method
+    
+script_dir = os.path.dirname(os.path.abspath(__file__)) 
+scenePath = os.path.join(script_dir, "coppeliasim_based_benchmark", f"{meth}.ttt")
+sim.setStringSignal("method", meth)
+env = environment_type
+sim.setStringSignal("environment", env)
+
+j = environment_num
+sim.setStringSignal("terrain_number", j)
+sim.loadScene(scenePath)
+time.sleep(1.5) 
+sim.startSimulation()
+while True:
+    sim.step()
+    simState = sim.getSimulationState()
+    if simState == sim.simulation_paused:
+        break
+    #time.sleep(0.1)  
+print(f"Simulation about {env},{meth},terrain number{j} is done.")    
+sim.stopSimulation()
